1e3b3e7b69f30083ac9f6bfa34f0a84802d78209
Justin Lowen <justin.lowen@l7informatics.com>
2024-10-18 15:21:30 -0500

    List examples - Cypress Learning
    Use of within, its methods.

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -9,8 +9,8 @@ describe('Various examples', () => {
         cy.location("pathname").should("equal", "/overview")
         cy.getDataTest('nav-fundamentals').click()
         cy.location("pathname").should("equal", "/fundamentals")
-    }),
-    it.only('intercepts', () => {
+    })
+    it('intercepts', () => {
         cy.intercept("POST", "http://localhost:3000/examples", {
             // body: {
             //     message: 'successfully intercepted request'
@@ -23,5 +23,52 @@ describe('Various examples', () => {
         }).as("signup")
         cy.getDataTest('postButton').click()
         cy.wait(['@signup'])
+    })
+    it.only('grudges', () => {
+        cy.contains(/Add Some Grudges/i)
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 0)
+        })
+        cy.getDataTest('clear-button').should('not.exist')
+
+        cy.getDataTest('grudge-input').within(() => {
+            cy.get('input').type('Not enough tests')
+        })
+        cy.getDataTest('add-grudge-button').click()
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 1)
+        })
+
+        cy.getDataTest('grudge-input').within(() => {
+            cy.get('input').type('grudge 2')
+        })
+        cy.getDataTest('add-grudge-button').click()
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 2)
+            cy.get('li').its(1).should('contains.text', 'grudge 2')
+        })
+        cy.getDataTest('grudge-list-title').should('contains.text', 'Grudges')
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').its(0).within(() => {
+                cy.get('button').click()
+            })
+        })
+        
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 1)
+            cy.get('li').its(0).should('contains.text', 'grudge 2')
+        })
+
+        cy.getDataTest('clear-button').click()
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 0)
+        })
+
     })
 })
